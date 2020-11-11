318c30af6d6d690a82bfa6deb67fa838d824e345
Anand <anands.kartha@gmail.com>
2020-11-10 22:36:43 -0500

    Minor style changes

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,10 @@
-import React, {useContext} from 'react';
-import { GlobalContext } from '../context/GlobalState';
+import React from 'react';
 
 export const Transaction = ({ transaction }) => {
-  const { deleteTransaction } = useContext(GlobalContext);
 
   return (
-    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-      {transaction.text} <span>${Math.abs(transaction.amount)}</span><button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
+    <li>
+      {transaction.text} <span>${Math.abs(transaction.amount)}</span>
     </li>
   )
 }
